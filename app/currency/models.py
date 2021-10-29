10889e6b30c0d35b0fded655c9217d0385927c3c
xVilandx <malabarvovk18@gmail.com>
2021-10-29 18:06:51 +0300

    HW8 done

--- app/currency/models.py
+++ app/currency/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Rate(models.Model):
     buy = models.DecimalField(max_digits=6, decimal_places=2)
     sale = models.DecimalField(max_digits=6, decimal_places=2)
-    created = models.DateTimeField()
+    created = models.DateTimeField(auto_now_add=True)
     type = models.CharField(max_length=3)   # noqa: A003
     source = models.CharField(max_length=25)
 
@@ -13,3 +13,8 @@ class ContactUs(models.Model):
     email_from = models.EmailField(max_length=60)
     subject = models.CharField(max_length=30)
     message = models.TextField()
+
+
+class Source(models.Model):
+    source_url = models.CharField(max_length=255)
+    name = models.CharField(max_length=64)
--- app/settings/urls.py
+++ app/settings/urls.py
@@ -1,5 +1,12 @@
-from currency.views import contact_list, rate_list
+from currency.views import (
+    contact_list, delete_rate,
+    delete_source, rate_create,
+    rate_details, rate_list,
+    source_create, source_details,
+    source_list, update_rate,
+    update_source,
 
+)
 from django.contrib import admin
 from django.urls import path
 
@@ -8,5 +15,15 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('rate/list/', rate_list),
-    path('contact/list/', contact_list)
+    path('contact/list/', contact_list),
+    path('rate/create/', rate_create),
+    path('rate/update/<int:pk>/', update_rate),
+    path('rate/delete/<int:pk>/', delete_rate),
+    path('rate/details/<int:pk>/', rate_details),
+    path('source/list/', source_list),
+    path('source/create/', source_create),
+    path('source/update/<int:pk>/', update_source),
+    path('source/delete/<int:pk>/', delete_source),
+    path('source/details/<int:pk>/', source_details),
+
     ]
